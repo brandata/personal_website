c76789fd0e983d1398416a2c8cf0a490600e1be1
Brandon <brandon.haw@gmail.com>
2019-03-21 16:37:24 -0400

    Updated project information

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -110,13 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
